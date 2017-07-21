56096e9f8f2b99d2ec965ecb2a28edc48a71a02c
Michael Burr <mburr@nightmare.com>
2017-07-20 22:21:44 -0700

    Update wreckless_shell_script.sh

--- wreckless_shell_script.sh
+++ wreckless_shell_script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# wget -O - https://raw.githubusercontent.com/stnbu/devenvbootstrap/master/wreckless_shell_script.sh | /bin/sh
+
 ~/git && cd ~/git/
 git -c http.sslVerify=false clone https://github.com/stnbu/devenvbootstrap.git
 cd devenvbootstrap/
