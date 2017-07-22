e30284960157574a7824646226c4033533862779
Mike Burr <mburr@vmware.com>
2017-07-22 15:37:28 -0700

    make the thing better

--- do_emacs.py
+++ do_emacs.py
@@ -19,16 +19,24 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-install_prefix = os.path.expanduser('~/.herp')
+INSTALL_PREFIX = os.path.expanduser('~/.herp')
 #import tempfile#temp_dir = tempfile.mkdtemp()
-source_root = os.path.join(os.path.expanduser('~/.herp'), 'source')
-archive_name = 'emacs.tar.xz'
-emacs_url = 'https://mirrors.ocf.berkeley.edu/gnu/emacs/emacs-25.2.tar.xz'
+SOURCE_ROOT = os.path.join(os.path.expanduser('~/.herp'), 'source')
+ARCHIVE_NAME = 'emacs.tar.xz'
+EMACS_URL = 'https://mirrors.ocf.berkeley.edu/gnu/emacs/emacs-25.2.tar.xz'
+
+PACKAGES = {
+    'ipython': {'debian': 'ipython'},
+    'pep8': {'debian': 'pep8'},
+    'autopep8': {'debian': 'python-autopep8'},
+    'virtualenv': {'debian': 'python-virtualenv'},
+}
 
 
-def extract_tarball(tarball_path, dst):
+def extract_tarball(tarball_path, dest_dir):
     logger.info('Extracting')
-    os.chdir(dst)
+    makedirs(dest_dir)
+    os.chdir(dest_dir)
     with tarfile.open(tarball_path) as f:
         f.extractall('.')
 
@@ -37,18 +45,34 @@ def get_version_string(archive_path_fragment):
     version_string, = m.groups()
     return version_string
 
+def makedirs(path):
+    if not os.path.exists(path):
+        logger.debug('%s did not exist. Creating.' % path)
+        os.makedirs(path)
 
-def get_downloaded_emacs_archive_path(dest_path):
-    logger.info('Downloading')
-    dest_path = os.path.abspath(dest_path)
-    if not os.path.exists(os.path.join(dest_path, archive_name)):
+
+def get_downloaded_emacs_archive(dest_dir, emacs_url):
+    logger.info('Downloading emacs source archive')
+    logger.debug('Destination: %s' % dest_dir)
+    logger.debug('URL: %s' emacs_url)
+    dest_dir = os.path.abspath(dest_dir)
+    makedirs(dest_dir)
+    dest_filename = os.path.join(dest_dir, archive_name)
+    if not os.path.exists(dest_filename):
         opener = request.URLopener()
-        opener.retrieve(emacs_url, os.path.join(dest_path, archive_name))
-    return dest_path, archive_name
+        try:
+            opener.retrieve(emacs_url, dest_filename)
+        except Exception as e:
+            logger.error('Download failed. Deleting archive %s' % dest_filename)
+            os.remove(dest_filename)
+            raise e
+    else:
+        logger.info('Emacs source archive there. Using that.')
+    return dest_filename
 
 
 def system(command):
-    #print(' '.join(command))
+    logger.debug('Executing command: %s' % ' '.join(command))
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     pout, perr = p.communicate()
     if p.returncode:
@@ -59,49 +83,106 @@ def system(command):
 def configure(path):
     logger.info('Configuring')
     os.chdir(path)
-    system(['./configure', '--prefix', install_prefix, '--without-x'])
+    system(['./configure', '--prefix', INSTALL_PREFIX, '--without-x'])
 
 def make(path, target=None):
     cmd = []
     cmd.append('make')
     if target is not None:
         cmd.append(target)
-    logger.info('make {0}'.format('' if target is None else target))
+    logger.info('make %s' % ('' if target is None else target))
     os.chdir(path)
     system(cmd)
 
+"""
+~/git && cd ~/git/
+git -c http.sslVerify=false clone https://github.com/stnbu/devenvbootstrap.git
+cd devenvbootstrap/
 
-if __name__ == '__main__':
+python3 do_emacs.py
+. ~/.bashrc
+emacs --daemon
+"""
 
-    if not os.path.exists(source_root):
-        os.makedirs(source_root)
-    tarball_dir, tarball_name = get_downloaded_emacs_archive_path(source_root)
-    tarball_path = os.path.join(tarball_dir, tarball_name)
-    emacs_source_dir = os.path.join(source_root, 'emacs')
-    if not os.path.exists(emacs_source_dir):
-        os.makedirs(emacs_source_dir)
-
+def get_extracted_source_root(base_dir, tarball_path):
+    base_dir = os.path.abspath(base_dir)
+    makedirs(base_dir)
     extract_tarball(tarball_path, emacs_source_dir)
-    this_version_source = os.path.join(emacs_source_dir, 'emacs-{0}'.format(get_version_string(emacs_url)))
+    emacs_source_dir, = glob(os.path.join(base_dir, 'emacs*'))
+    return emacs_source_dir
 
-    configure(this_version_source)
-    make(this_version_source)
-    make(this_version_source, 'install')
 
     dot_emacs = os.path.expanduser('~/.emacs')
     dot_bashrc = os.path.expanduser('~/.bashrc')
-    if not os.path.exists(dot_emacs):
-        os.symlink('git/devenvbootstrap/dot.emacs', dot_emacs)
+
+def link_dot_emacs(path):
+    dot_emacs_path = os.path.expanduser('~/.emacs')
+    if os.path.exists(dot_emacs_path) and os.path.islink(dot_emacs_path):
+        os.remove(path)
+    else:
+        logger.warn('~/.emacs already exists.')
+        return
+    os.symlink('git/devenvbootstrap/dot.emacs', dot_emacs)
+
+def update_dot_bashrc():
+    dot_bashrc_path = os.path.expanduser('~/.bashrc')
+    if not os.path.exists(dot_bashrc_path):
+        with open(dot_bashrc_path, 'w') as f:
+            f.close()
     if 'devenvbootstrap' not in open(dot_bashrc).read():
         with open(dot_bashrc, 'a') as f:
             f.write('\n. ~/git/devenvbootstrap/dot.bashrc\n')
 
-    # doesn't even afraid
-    system(['apt-get', '-yy', 'install', 'ipython', 'pep8', 'python-autopep8'])
-    system(['pip', 'install', 'remote-pdb'])
+def get_platform():
+    name, _, _ = platform.dist()
+    return name.lower()
 
+
+def install_system_packages(package_aliases):
+    platform_name = get_platform()
+    to_install = [v[platform_name] for k, v in PACKAGES.iteritems() if k in package_aliases]
+    to_install = ' '.join(to_install)
+    command = 'apt-get -y -q install %s'.split()
+    command.extend(to_install)
+    system(install_command)
+
+
+def install_global_python_packages(package_names):
+    command = 'pip install'.split()
+    command.extend(package_names)
+    system(command)
+
+def clone_repo(uri, dest_dir):
+    logger.info('Cloning repository.')
+    dest_dir = os.path.abspath(dest_dir)
+    makedirs(dest_dir)
+    command = 'git -c http.sslVerify=false clone'.split()
+    command.append(uri)
+    command.append(dest_dir)
+    system(command)
+
+if __name__ == '__main__':
+
+    clone_repo('https://github.com/stnbu/devenvbootstrap.git', os.path.expanduser('~/git/devenvbootstrap'))
+
+    tarball_path = get_downloaded_emacs_archive_path(SOURCE_ROOT)
+    emacs_source_dir = get_extracted_source_root(base_dir, tarball_path)
+
+    configure(emacs_source_dir)
+    make(this_version_source)
+    make(this_version_source, 'install')
+
+    link_dot_emacs('/root/git/devenvbootstrap/dot.emacs')
+    update_dot_bashrc()
+
+    install_system_packages('ipython', 'pep8', 'autopep8', 'virtualenv')
+    install_global_python_packages('remote-pdb')
+
+
+    """
     id_rsa_pub = open(os.path.expanduser('~/git/devenvbootstrap/id_rsa.pub'), 'r').read().strip()
     authorized_keys_path = os.path.expanduser('~/.ssh/authorized_keys')
     if id_rsa_pub not in open(authorized_keys_path, 'r').read():
         with open(authorized_keys_path, 'a') as f:
             f.write('\n'+id_rsa_pub+'\n')
+    """
